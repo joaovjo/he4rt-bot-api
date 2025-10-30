20a7c38d6dc6ef3e6d0dd197d3bd03b4763d8f68
danielhe4rt <danielhe4rt@gmail.com>
2025-10-30 00:02:58 -0300

    refactor: update configuration files for consistency and optimize imports

--- config/he4rt.php
+++ config/he4rt.php
@@ -1,6 +1,6 @@
 <?php
 
-use Carbon\Carbon;
+use Illuminate\Support\Facades\Date;
 
 return [
     'season' => [
@@ -20,8 +20,8 @@ return [
         [
             'name' => 'Legado 4Y',
             'description' => 'A primeira temporada que dura desde o inicio da He4rt Developers.',
-            'starts_at' => Carbon::parse('2018-08-01'),
-            'ends_at' => Carbon::parse('2022-12-31'),
+            'starts_at' => Date::parse('2018-08-01'),
+            'ends_at' => Date::parse('2022-12-31'),
             'messages_count' => 0,
             'participants_count' => 0,
             'meetings_count' => 0,
@@ -30,8 +30,8 @@ return [
         [
             'name' => 'He4rt Shippuden',
             'description' => 'Segunda temporada chegando foda memo dps nois acha um nome melhor pra isso aqui.',
-            'starts_at' => Carbon::parse('2023-01-01'),
-            'ends_at' => Carbon::parse('2023-12-31'),
+            'starts_at' => Date::parse('2023-01-01'),
+            'ends_at' => Date::parse('2023-12-31'),
             'messages_count' => 0,
             'participants_count' => 0,
             'meetings_count' => 0,
